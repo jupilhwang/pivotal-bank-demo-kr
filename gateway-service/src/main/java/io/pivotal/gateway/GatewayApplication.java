b7a7ba4425d7309b24016360c085cd406495e832
Jupil Hwang <jupil.hwang@gmail.com>
2019-12-04 22:12:52 +0900

    chagned

--- gateway-service/src/main/java/io/pivotal/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/io/pivotal/gateway/GatewayApplication.java
@@ -2,14 +2,31 @@ package io.pivotal.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class GatewayApplication {
+	@Bean
+	public RouteLocator customGatewayRouteLoator(RouteLocatorBuilder builder)	{
+		return builder.routes()
+				.route("quotes_route", r -> r.path("/v1/**")
+						.filters(f -> f.addResponseHeader("X-Spring-Cloud-Meetup-Pivotal", "HelloWorld-Welcome"))
+						.uri("lb://quotes-service"))
+				.build();
+	}
+
+	@RequestMapping("/myfallback")
+	public String myfallback()	{
+		return "Hello World !!! Fallback";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
--- gateway-service/src/main/java/io/pivotal/gateway/config/SecurityConfig.java
+++ gateway-service/src/main/java/io/pivotal/gateway/config/SecurityConfig.java
@@ -2,19 +2,30 @@ package io.pivotal.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
+//@Configuration
+//public class SecurityConfig {
+//    @Bean
+//    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+//        return http
+//                .csrf().disable()
+//                .authorizeExchange()
+//                .anyExchange().permitAll()
+//                .and()
+//                .build();
+//    }
+//
+//}
+//
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .authorizeExchange()
-                .anyExchange().permitAll()
-                .and()
-                .build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http)    {
+        http.authorizeExchange().anyExchange().permitAll();
+        return http.build();
     }
-
 }
+
